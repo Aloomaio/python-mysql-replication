ee440d90f672555bcfb0f7b557da1aa7d6a7f7d7
sagi yosefia <7414322+sagiyosefia@users.noreply.github.com>
2021-01-06 17:30:39 +0200

    Revert "Sync from Master Repo"

--- pymysqlreplication/row_event.py
+++ pymysqlreplication/row_event.py
@@ -238,8 +238,7 @@ class RowsEvent(BinLogEvent):
     def __read_string(self, size, column):
         string = self.packet.read_length_coded_pascal_string(size)
         if column.character_set_name is not None:
-            encoding = self.charset_to_encoding(column.character_set_name)
-            string = string.decode(encoding)
+            string = string.decode(charset_to_encoding(column.character_set_name))
         return string
 
     def __read_bit(self, column):
